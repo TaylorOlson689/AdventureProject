fffb7df0f380a4c5a9aa8bcd3962ede529b4f2e2
taylolso689 <taylolso689@SNS-D-130S09>
2022-03-28 13:37:40 -0400

    Add project files.

---
+++ AdventureProject/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace AdventureProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.option1Button = new System.Windows.Forms.Button();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Location = new System.Drawing.Point(63, 22);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(477, 32);
+            this.outputLabel.TabIndex = 2;
+            // 
+            // option1Button
+            // 
+            this.option1Button.BackColor = System.Drawing.Color.GreenYellow;
+            this.option1Button.Location = new System.Drawing.Point(63, 270);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(477, 33);
+            this.option1Button.TabIndex = 3;
+            this.option1Button.Text = "option1";
+            this.option1Button.UseVisualStyleBackColor = false;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // option2Button
+            // 
+            this.option2Button.BackColor = System.Drawing.Color.GreenYellow;
+            this.option2Button.Location = new System.Drawing.Point(63, 321);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(477, 33);
+            this.option2Button.TabIndex = 4;
+            this.option2Button.Text = "option2";
+            this.option2Button.UseVisualStyleBackColor = false;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // imageBox
+            // 
+            this.imageBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.imageBox.Location = new System.Drawing.Point(152, 78);
+            this.imageBox.Margin = new System.Windows.Forms.Padding(2);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(276, 177);
+            this.imageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox.TabIndex = 12;
+            this.imageBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(663, 382);
+            this.Controls.Add(this.imageBox);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "Form1";
+            this.Text = "Adventure Game";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.PictureBox imageBox;
+    }
+}
+
---
+++ AdventureProject/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace AdventureProject
+{ 
+    public partial class Form1 : Form
+    {
+        int page = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            DisplayPage();
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+           
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 3;
+            }
+            else if (page == 3)
+            {
+                page = 4;
+            }
+            else if (page == 4)
+            {
+                page = 5;
+            }
+            else if (page == 5)
+            {
+                page = 99;
+            }
+
+            DisplayPage();
+
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 6;
+            }
+            else if (page == 6)
+            {
+                page = 7;
+            }
+          
+
+            DisplayPage();
+
+        }
+
+        public void DisplayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    outputLabel.Text = "Welcome to the Stratford VIA/GO Train Station. Ready for an adventure? Which provider?";
+                    option1Button.Text = "VIA Rail";
+                    option2Button.Text = "GO Transit";
+
+                    imageBox.Image = Properties.Resources.StratfordVIAStation;
+                    break;
+                case 2:
+                    outputLabel.Text = "You just boarded VIA Rail. Which driection?";
+                    option1Button.Text = "Westbound towards Sarnia";
+                    option2Button.Text = "Eastbound towards Toronto";
+
+                    imageBox.Image = Properties.Resources.StratfordVIATrain;
+                    SoundPlayer player2 = new SoundPlayer(Properties.Resources.Horn);
+                    player2.Play();
+
+                    break;
+                case 3:
+                    outputLabel.Text = "Which Class?";
+                    option1Button.Text = "Business";
+                    option2Button.Text = "Economy";
+                    break;
+                case 4:
+                    outputLabel.Text = "You made it to your destination. Return trip?";
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    break;
+                case 5:
+                    outputLabel.Text = "You made it!";
+                    option1Button.Text = "";
+                    option2Button.Text = "";
+                    break;
+                case 6:
+                    outputLabel.Text = "You just boarded GO Transit. Which direction?";
+                    option1Button.Text = "Westbound towards London";
+                    option2Button.Text = "Eastbound towards Toronto";
+                    imageBox.Image = Properties.Resources.StratfordGOTrain;
+                    SoundPlayer player3 = new SoundPlayer(Properties.Resources.Horn);
+                    player3.Play();
+                    break;
+                case 7:
+                    outputLabel.Text = "You made it to your destination. Return trip?";
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    break;
+                case 99:
+                    outputLabel.Text = "Thank you for playing. Play again?";
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
